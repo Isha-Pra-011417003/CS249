2f54cfab02159c6df95953cc75774b78a9185204
isha-pradhan <ishap@credible.com>
2017-10-16 19:18:15 -0700

    Code changes - minor

--- HW6_ChandyLamport/Main.java
+++ HW6_ChandyLamport/Main.java
@@ -9,85 +9,61 @@ import java.util.List;
  */
 public class Main {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        //Channels from P3 to P1 and P2 to P1
-        Buffer channelP31 = new Buffer("P31");
-        Buffer channelP21 = new Buffer("P21");
+		// Channels from P3 to P1 and P2 to P1
+		Buffer channelP31 = new Buffer("P31");
+		Buffer channelP21 = new Buffer("P21");
 
-        //Channels from P3 to P2 and P1 to P2
-        Buffer channelP32 = new Buffer("P32"); //source destination
-        Buffer channelP12 = new Buffer("P12"); //source Dest
+		// Channels from P3 to P2 and P1 to P2
+		Buffer channelP32 = new Buffer("P32"); // source destination
+		Buffer channelP12 = new Buffer("P12"); // source Dest
 
-        //Channels from P2 to P3 and P1 to P3
-        Buffer channelP23 = new Buffer("P23");
-        Buffer channelP13 = new Buffer("P13");
+		// Channels from P2 to P3 and P1 to P3
+		Buffer channelP23 = new Buffer("P23");
+		Buffer channelP13 = new Buffer("P13");
 
+		List<Buffer> inChannelsP1 = new ArrayList<>();
+		inChannelsP1.add(channelP31);
+		inChannelsP1.add(channelP21);
+		List<Buffer> outChannelsP1 = new ArrayList<>();
+		outChannelsP1.add(channelP13);
+		outChannelsP1.add(channelP12);
+		Processor processor1 = new Processor(1, inChannelsP1, outChannelsP1); // Only observes in channels.
 
-        List<Buffer> inChannelsP1 = new ArrayList<>();
-        inChannelsP1.add(channelP31);
-        inChannelsP1.add(channelP21);
-        List<Buffer> outChannelsP1 = new ArrayList<>();
-        outChannelsP1.add(channelP13);
-        outChannelsP1.add(channelP12);
-        Processor processor1 = new Processor(1, inChannelsP1, outChannelsP1); //Only observes in channels.
+		List<Buffer> inChannelsP2 = new ArrayList<>();
+		inChannelsP2.add(channelP12);
+		inChannelsP2.add(channelP32);
+		List<Buffer> outChannelsP2 = new ArrayList<>();
+		outChannelsP2.add(channelP21);
+		outChannelsP2.add(channelP23);
+		Processor processor2 = new Processor(2, inChannelsP2, outChannelsP2); // Only observes in channels.
 
-        List<Buffer> inChannelsP2 = new ArrayList<>();
-        inChannelsP2.add(channelP12);
-        inChannelsP2.add(channelP32);
-        List<Buffer> outChannelsP2 = new ArrayList<>();
-        outChannelsP2.add(channelP21);
-        outChannelsP2.add(channelP23);
-        Processor processor2 = new Processor(2, inChannelsP2, outChannelsP2); //Only observes in channels.
+		List<Buffer> inChannelsP3 = new ArrayList<>();
+		inChannelsP3.add(channelP13);
+		inChannelsP3.add(channelP23);
+		List<Buffer> outChannelsP3 = new ArrayList<>();
+		outChannelsP3.add(channelP31);
+		outChannelsP3.add(channelP32);
+		Processor processor3 = new Processor(3, inChannelsP3, outChannelsP3); // Only observes in channels.
 
+		/**
+		 * Choose one processor to initiale a snapshot. Please note that any processor
+		 * has the capability to initiate a snapshot. //TODO: Homework: initiate
+		 * snapshot [Hint: call the initiateSnapshot method ]
+		 */
 
-        List<Buffer> inChannelsP3 = new ArrayList<>();
-        inChannelsP3.add(channelP13);
-        inChannelsP3.add(channelP23);
-        List<Buffer> outChannelsP3 = new ArrayList<>();
-        outChannelsP3.add(channelP31);
-        outChannelsP3.add(channelP32);
-        Processor processor3 = new Processor(3, inChannelsP3, outChannelsP3); //Only observes in channels.
+		Algorithm algo1 = new Algorithm(processor1, processor2, processor3, 1);
+		Algorithm algo2 = new Algorithm(processor1, processor2, processor3, 2);
+		Algorithm algo3 = new Algorithm(processor1, processor2, processor3, 3);
 
-        /**
-         * Choose one processor to initiale a snapshot. Please note that any processor has the capability to
-         * initiate a snapshot.
-         * //TODO: Homework: initiate snapshot
-         * [Hint: call the initiateSnapshot method ]
-         */
+		algo1.start();
+		algo2.start();
+		algo3.start();
 
-        Algorithm algo1 = new Algorithm(processor1, processor2, processor3, 1);
-        Algorithm algo2 = new Algorithm(processor1, processor2, processor3, 2);
-        Algorithm algo3 = new Algorithm(processor1, processor2, processor3, 3);
-        
-        
-        algo1.start();
-        algo2.start();
-        algo3.start();
-        try {
-          
-        Thread.sleep(2000);
-        
-//        processor1.initiateSnapShot();
-          System.out.println("Initiating snapshot from P1");
-          processor1.initiateSnapShot();
-         
-        
-          Thread.sleep(7000);
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        
-       
-        
-//        i++;
-//       }
-        
-        
-        
+		System.out.println("Initiating snapshot from P1");
+		processor1.initiateSnapShot();
 
-    }
-
+	}
 
 }
--- HW6_ChandyLamport/RecordApplicationMessages.java
+++ HW6_ChandyLamport/RecordApplicationMessages.java
@@ -6,82 +6,42 @@ import java.util.List;
 import java.util.Map;
 
 public class RecordApplicationMessages extends Thread {
-  Buffer channel;
-  Processor p;
-  Map <Buffer, Integer> lastIndexMap = null;
-  
-  public RecordApplicationMessages(Buffer channel, Processor p) {
-    this.channel = channel;
-    this.p=p;
-    lastIndexMap = new HashMap <Buffer, Integer> ();
-  }
-  
+	Buffer channel;
+	Processor p;
+	List<Message> recordedMessagesSinceMarker;
 
-  
-  public void run() {
-    
-    int lastIdx=-1;
-    
-    System.out.println();
-    if(!lastIndexMap.containsKey(channel)) {
-      lastIdx = channel.getTotalMessageCount();
-      lastIndexMap.put(channel, lastIdx);
-     } else
-       lastIdx = lastIndexMap.get(channel);
-    recordChannel(channel, lastIdx);  
-  }
-  
-  
-    public void recordChannel(Buffer channel, int lastIdx) {
-    
-    List<Message> recordedMessagesSinceMarker = new ArrayList<>();
-    
-    try {
-      while (p.channelMarkerCount.get(p)==1) {
-        
-        System.out.println("@@@@@@ I started recording on channel " + channel.getLabel() + " after index: " + lastIdx);
-        System.out.println(" Channel " + channel.getLabel() + " Message count is: " + channel.getTotalMessageCount());
-        if(channel.getMessage(lastIdx)!=null)
-          recordedMessagesSinceMarker.add(channel.getMessage(lastIdx));
-       
-     
-      System.out.println();
-      System.out.println("The channelmarkercount is no more just 1. Its more!");
-      p.channelState.put(channel, recordedMessagesSinceMarker);
-//      System.out.println("INTERRUPTED: Channel msg count before was: " + lastIdx + ". Channel msg count now is: " + channel.getTotalMessageCount());
-//      System.out.println("Interrupted channel " + channel.getLabel() + ". Put the channel and its message in the global state.");
-      System.out.println();
-      }
-    } catch (Exception e) {
-      
-      throw new RuntimeException("Recording of channel " + channel.getLabel() + " interrupted...");
-    } 
-    
-    
-    
-//    try {
-//      while (p.channelMarkerCount.get(p)==1) {
-//        i= count;
-//        while(i<=count) {
-//          System.out.println("~~~~~~~~~~~~~~~~~~~ For channel" + channel.getLabel()+ "i is: " + i);
-//          System.out.println("___________________ count for channel" + channel.getLabel()+ " is: " + count);
-//          recordedMessagesSinceMarker.add(channel.getMessage(i));
-//          i++;
-//          count = channel.getTotalMessageCount();
-//          Thread.sleep(3000);
-//        }
-//       
-//      }
-//      p.channelState.put(channel, recordedMessagesSinceMarker);
-//      
-//    } catch (InterruptedException e) {
-//      throw new RuntimeException("Recording of channel " + channel + " interrupted...");
-//    }
-    
-    
-    
- 
+	public RecordApplicationMessages(Buffer channel, Processor p) {
+		this.channel = channel;
+		this.p = p;
+		recordedMessagesSinceMarker = new ArrayList<>();
+	}
 
-  }
+	@Override
+	public void run() {
+		int lastIdx = channel.getTotalMessageCount() - 1;
+		int current=lastIdx;
+		
+		while (!this.isInterrupted()) {
+		  int count = channel.getTotalMessageCount() - 1;
+			if (current < count) {
+				
+				Message message = channel.getMessage(current);
 
+				if (MessageType.MARKER.equals(message.getMessageType())) {
+					System.out.println("*********Duplicate marker received on channel "+channel.getLabel()+"************");
+					System.out.println("Interrupting thread "+Thread.currentThread().getName());
+					Thread.currentThread().interrupt();
+					break;
+				} 
+			} else {
+				try {
+				  current++;
+					Thread.currentThread().sleep(1000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
 }
